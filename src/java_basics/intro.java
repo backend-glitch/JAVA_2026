cb337a3705c7fad12fd24323003a8b8109b3b18d
Arjun_2025 <vineetaarjun123@gmail.com>
2026-03-26 20:05:12 +0530

    added 1_intre

---
+++ src/java_basics/intro.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+//day 1
+
+public class intro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+
+        //data types
+
+    }
+
+}
